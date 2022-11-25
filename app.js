b1b3bd9c9d52b59de5e7e266e7b69fbadbf55c29
huseyin corakli <huseyincorakli@icloud.com>
2022-11-26 00:04:16 +0300

    static files uploaded

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-const fn = () => {
-  console.log('çalıştı');
-};
-fn();
+const express = require('express');
+const app = express();
+const path = require('path')
+const port = 8888;
+
+//MIDDLEWARES
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname,"temp/index.html"))
+});
+
+app.listen(port, () => {
+  console.log(`${port} aktif`);
+});
