6367e56794322261b36403f590f18ad090653318
huseyin corakli <huseyincorakli@icloud.com>
2022-11-26 15:30:45 +0300

    views created

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
 const express = require('express');
+const path = require('path');
+const ejs = require('ejs');
 const app = express();
-const path = require('path')
 const port = 8888;
 
-//MIDDLEWARES
+/**const logger= (req,res,next)=>{
+    console.log("log 1");
+    // next();
+}
+* ! eğer next demez ise sonraki middleware çalışmayacaktır
+*/
+
+//? TEMPLATE ENGİNE
+app.set('view engine', 'ejs');
+
+//? MIDDLEWARES
 app.use(express.static('public'));
+// app.use(logger);
+
+//? ROUTES
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname,"temp/index.html"))
+  res.render('index');
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/add', (req, res) => {
+  res.render('add');
 });
 
 app.listen(port, () => {
