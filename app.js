4e56dfdf426cbec0fdb95117aa34b850798e4f23
huseyin corakli <huseyincorakli@icloud.com>
2022-11-27 00:31:49 +0300

    add data to db

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const mongoose= require('mongoose')
 const path = require('path');
 const ejs = require('ejs');
-const app = express();
+const Photo=require('./models/Photo');
 const port = 8888;
+
+const app = express();
+mongoose.connect('mongodb://127.0.0.1:27017/pcat-test2-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+
 
 /**const logger= (req,res,next)=>{
     console.log("log 1");
@@ -16,11 +25,18 @@ app.set('view engine', 'ejs');
 
 //? MIDDLEWARES
 app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 // app.use(logger);
 
 //? ROUTES
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async(req, res) => {
+  const photos= await Photo.find();
+  res.render('index',{photos});
+});
+app.get('/photos/:id', async(req, res) => {
+  const photo= await Photo.findById(req.params.id);
+  res.render('photo',{photo});
 });
 app.get('/about', (req, res) => {
   res.render('about');
@@ -28,6 +44,10 @@ app.get('/about', (req, res) => {
 app.get('/add', (req, res) => {
   res.render('add');
 });
+app.post('/photos',async(req,res)=>{
+  await Photo.create(req.body)
+  res.redirect('/');
+})
 
 app.listen(port, () => {
   console.log(`${port} aktif`);
